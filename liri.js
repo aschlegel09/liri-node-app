32524f0e823e5a90d24a1dccd2b50a87c7bf5aa8
aschlegel <aschlegel09@gmail.com>
2018-10-08 18:32:51 -0400

    first commit

---
+++ liri.js
@@ -0,0 +1,7 @@
+require("dotenv").config();
+
+var spotify = new Spotify(keys.spotify);
+
+http://www.omdbapi.com/?apikey=cd8c7793&
+
+"https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
