0b2ec27c66efeb250bde7fc77ffbe86f76c507f9
aschlegel <aschlegel09@gmail.com>
2018-10-13 13:01:47 -0400

    final commit with working code

--- liri.js
+++ liri.js
@@ -2,15 +2,17 @@ require("dotenv").config();
 
 var request = require("request");
 
+var fs = require("fs");
+
 var myKeys = require("./keys.js");
 
 var Spotify = require("node-spotify-api");
 
-var fs = require('fs'); 
-
 var spotify = new Spotify(myKeys.spotify);
 
 var userInput = process.argv[2];
+
+var userQuery = process.argv.slice(3).join(" ");
 
 if (
   userInput === "spotify-this-song" ||
@@ -19,15 +21,30 @@ if (
   userInput === "do-what-it-says"
 ) {
   console.log("\nYour process is: " + process.argv[2] + "\n");
-} else {
+}
+else if (userInput === "spotify-this-song" || !userInput) {
+  userQuery = "Ace-of-Base";
+  userInput = "spotify-this-song";
+} 
+else {
   console.log("Please enter a valid process.");
 }
 
-var userQuery = process.argv[3];
-
 if (userQuery) {
   console.log("Your search term is: " + userQuery + "\n");
-} else {
+} 
+else if (userInput === "movie-this") {
+  userQuery = "Mr. Nobody";
+  userInput = "movie-this";
+} 
+else if (userInput === "do-what-it-says"){
+  console.log("OK!");
+} 
+else if (userInput === "spotify-this-song") {
+  userQuery = "Ace-of-Base";
+  userInput = "spotify-this-song";
+}
+else {
   console.log("Please enter a search term.");
 }
 
@@ -38,26 +55,22 @@ printMovie = function() {
       if (error) {
         console.log("Movie error!!");
       }
-      // console.log("Line28error:", error); // Print the error if one occurred
-      // console.log("statusCode:", response && response.statusCode); // Print the response status code if a response was received
-      // console.log("Response: " + JSON.stringify(response));
-      // console.log(body.body);
-      
-   var mainBandBody = body.body;
-   // console.log(mainBandBody);
-   
-   var str = mainBandBody;
-   var newString = str.replace(/[^a-zA-Z ]/g, "");
-   console.log("Good luck with this:... " + newString.split("  "));
-      // console.log(Object.entries(response.body));
-      // for (var [key, value] of Object.entries(response)) {
-      //   console.log(`${key} ${value}`);
-      // }
+      var movieBody = body.body;
+      // console.log(typeof movieBody);
+
+      var parsedString = JSON.parse(movieBody);
+      // console.log(parsedString);
+      console.log("Name of movie: " + parsedString.Title); //WORKS
+      console.log("Year Released: " + parsedString.Year);
+      console.log("IMDB Rating: " + parsedString.imdbRating);
+      console.log("Rotten Tomatoes Rating: " + parsedString.Ratings[1].Value);
+      console.log("Country: " + parsedString.Country);
+      console.log("Language: " + parsedString.Language);
+      console.log("Plot: " + parsedString.Plot);
+      console.log("Actors: " + parsedString.Actors);
     }
   );
 };
-
-// var defaultArtist = "Ace-of-Base";
 
 printSong = function() {
   {
@@ -83,26 +96,14 @@ printSong = function() {
   }
 };
 
-// var apiInstance = new BandsintownApi.ArtistEventsApi();
-
-// var artistname = "artistname_example"; // String | The name of the artist. If it contains one of the special characters below, please be sure to replace it by the corresponding code: for / use %252F, for ? use %253F, for * use %252A, and for \" use %27C
-
-// var appId = "appId_example"; // String | The application ID assigned to you by Bandsintown
-
-// var opts = {
-//   _date: "_date_example" // String | Can be one of the following values: \"upcoming\", \"past\", \"all\", or a date range e.g. \"2015-05-05,2017-05-05\". If not specified, only upcoming shows are returned
-// };
-
 var moment = require("moment");
-
-moment().format();
 
 printBand = function() {
   // Then run a request to the OMDB API with the movie specified
   var bandUrl =
     "https://rest.bandsintown.com/artists/" +
     userQuery +
-    "?app_id=codingbootcamp";
+    "/events?app_id=codingbootcamp";
 
   // This line is just to help us debug against the actual URL.
   // console.log(bandUrl);
@@ -111,57 +112,43 @@ printBand = function() {
     if (error) {
       // Print the error if one occurred
       console.log("Song Error: " + error);
-    };
-  
-    // console.log(response.statusCode); // Print the response status code if a response was received
-    // console.log(response.body.name);
-    // console.log(response.body);
-   var mainBandBody = response.body;
-    // console.log(mainBandBody);
-    
-    var str = mainBandBody;
-    console.log(str.replace(/[^a-zA-Z ]/g, ""));
+    }
+    var concerts = JSON.parse(body);
+    console.log("Name of Venue: " + concerts[0].venue.name); // WORKING NAME
+    console.log("City: " + concerts[0].venue.city);
+    console.log("Region: " + concerts[0].venue.region);
+    console.log("Country: " + concerts[0].venue.country);
 
+    var dateOfConcert = concerts[0].datetime;
+
+    var momentDate = moment(dateOfConcert).format("MM/DD/YYYY");
+
+    console.log("Date of Concert: " + momentDate);
   });
-  // var callback = function(error, data, response) {
-  //   if (error) {
-  //     console.error(error);
-  //   } else {
-  //     console.log("API called successfully. Returned data: " + data);
-  //     console.log(response);
-  //   }callback();
-  // };
-  // apiInstance.artistEvents(artistname, appId, opts, callback);
 };
-var randomText = require("./random.txt");
 
 whatISaid = function() {
+  fs.readFile("random.txt", function(err, data) {
+    if (err) throw err;
+    // convert data object to a string
+    var newData = data.toString();
 
-  fs.readFile(randomText, function(err, data) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
-    res.end();
+    var splitData = newData.split(",");
+    // console.log(splitData);
 
+    userInput = splitData[0];
+    userQuery = splitData[1];
+    printSong();
   });
- 
-var newUserInput = randomText.split("");
-
-console.log(newUserInput);
-
-  printSong();
-;}
+};
 
 if (userInput === "movie-this") {
-  // console.log("movie-this chosen");
   printMovie();
 } else if (userInput === "spotify-this-song") {
-  // console.log("spotify-this-song chosen");
   printSong();
 } else if (userInput === "concert-this") {
-  // console.log("concert-this chosen");
   printBand();
 } else if (userInput === "do-what-it-says") {
-  // console.log("concert-this chosen");
   whatISaid();
 } else {
   console.log("error: no process chosen");
